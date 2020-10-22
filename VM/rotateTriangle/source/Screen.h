36462e38e046bb4d05710d6bdb2b095cb28eb717
Homry <Sorokumov.g.2019@fml31.ru>
2020-10-22 14:51:51 +0300

    add VM/rotateTriangle

---
+++ VM/rotateTriangle/source/Screen.h
@@ -0,0 +1,103 @@
+#ifndef ROTATETRIANGLE_SCREEN_H
+#define ROTATETRIANGLE_SCREEN_H
+
+#include "RigidBody.h"
+#include <GL/glut.h>
+#include <math.h>
+#include <ctime>
+
+[[maybe_unused]] RigidBody body;
+double DeltaTime;
+
+void drawTriangle(){
+    glBegin(GL_TRIANGLES);
+    glColor3f(0.52,0.44,1.0);// Сделали боковую сторону фиолетовой
+    glVertex3f( 1.0, -1.0,-1.0);
+    glVertex3f(1.0, -1.0,1.0);
+    glVertex3f(0.0, 1.0, 0.0);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0,0.84,0.0);  // Сделали боковую сторону желтой
+    glVertex3f( 1.0,-1.0, 1.0);
+    glVertex3f(-1.0,-1.0, 1.0);
+    glVertex3f(0.0,1.0,0.0);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+    glColor3f(0.94,0.5,0.5);// Сделали сторону  розовой
+    glVertex3f(-1.0,-1.0,1.0);
+    glVertex3f(-1.0, -1.0,-1.0);
+    glVertex3f(0.0,1.0,0.0);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+    glColor3f(0.0,1.0,0.0);  // Сделали сторону  светло зеленой
+    glVertex3f(-1.0,-1.0,-1.0);
+    glVertex3f(1.0,-1.0,-1.0);
+    glVertex3f(0.0, 1.0,0.0);
+    glEnd();
+
+    glBegin(GL_QUADS);// основание пирамиды
+    glColor3f(1.0,0.51,0.28); // сделали основание рыжим
+    glVertex3f( 1.0,-1.0, 1.0);
+    glVertex3f(-1.0,-1.0, 1.0);
+    glVertex3f(-1.0,-1.0,-1.0);
+    glVertex3f( 1.0,-1.0,-1.0);
+    glEnd();
+};
+
+
+void Idle() {
+    long Time;
+    static long OldTime = -1;
+    static long StartTime;
+    if (OldTime == -1)
+        StartTime = OldTime = clock();
+    else {
+        Time = clock()*1.05;
+        DeltaTime = (double)(Time - OldTime) / CLOCKS_PER_SEC;
+        OldTime = clock();
+    }
+    glutPostRedisplay();
+};
+
+void screen(){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    body.solveRungeKutta(DeltaTime);
+    glPushMatrix();
+    glTranslated(body.x.x, body.x.y, body.x.z);
+    glRotatef(2*acos(body.q.r)*180/M_PI,  body.q.i, body.q.j, body.q.k);
+    glPolygonMode(GL_FRONT, GL_FILL);
+    drawTriangle();
+    glPopMatrix();
+    glFlush();
+    glutSwapBuffers();
+}
+
+void Reshape(int W, int H) {
+    glViewport(0, 0, W, H);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(15.0, ((float)W)/((float)H), 10.0, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0, 30, 0, 0, 0, 0, 0, 0, 1);
+}
+
+void init(){
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowSize(1920, 1080);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("result");
+    glutReshapeFunc(Reshape);
+    glutDisplayFunc(screen);
+    glutIdleFunc(Idle);
+    glClearColor(0, 0, 0, 0);
+    glEnable(GL_DEPTH_TEST);
+    glShadeModel(GL_SMOOTH);
+    glutMainLoop();
+}
+
+
+#endif //ROTATETRIANGLE_SCREEN_H

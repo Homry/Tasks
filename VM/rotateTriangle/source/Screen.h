31981c08d06d8d145b060e26e1ab87d60320d45a
Sergey Sorokumov <54909102+Homry@users.noreply.github.com>
2020-11-18 16:50:14 +0300

    Update Screen.h

--- VM/rotateTriangle/source/Screen.h
+++ VM/rotateTriangle/source/Screen.h
@@ -10,45 +10,44 @@
 [[maybe_unused]] RigidBody* body = new RigidBody();
 double DeltaTime;
 
-void drawTriangle(){
+void drawTriangle() {
 
     glBegin(GL_TRIANGLES);
-    glColor3f(0.52,0.44,1.0);// Сделали боковую сторону фиолетовой
-    glVertex3f((body->length/2), (body->width/2), 0.0);
-    glVertex3f((body->length/2), -(body->width/2), 0.0);
-    glVertex3f(0, 0, body->height);
+    glColor3f(0.52, 0.44, 1.0);// Сделали боковую сторону фиолетовой
+    glVertex3f((body->length / 2), (body->width / 2), -body->height/4.0);
+    glVertex3f((body->length / 2), -(body->width / 2), -body->height/4.0);
+    glVertex3f(0, 0, body->height*3.0/4.0);
     glEnd();
 
     glBegin(GL_TRIANGLES);
-    glColor3f(1.0,0.84,0.0);  // Сделали боковую сторону желтой
-    glVertex3f((body->length/2), -(body->width/2), 0.0);
-    glVertex3f(-(body->length/2), -(body->width/2), 0.0);
-    glVertex3f(0, 0, body->height);
+    glColor3f(1.0, 0.84, 0.0);  // Сделали боковую сторону желтой
+    glVertex3f((body->length / 2), -(body->width / 2), -body->height/4.0);
+    glVertex3f(-(body->length / 2), -(body->width / 2), -body->height/4.0);
+    glVertex3f(0, 0, body->height*3.0/4.0);
     glEnd();
 
     glBegin(GL_TRIANGLES);
-    glColor3f(0.94,0.5,0.5);// Сделали сторону  розовой
-    glVertex3f(-(body->length/2), -(body->width/2), 0.0);
-    glVertex3f(-(body->length/2), (body->width/2), 0.0);
-    glVertex3f(0.0, 0.0, body->height);
+    glColor3f(0.94, 0.5, 0.5);// Сделали сторону  розовой
+    glVertex3f(-(body->length / 2), -(body->width / 2), -body->height/4.0);
+    glVertex3f(-(body->length / 2), (body->width / 2), -body->height/4.0);
+    glVertex3f(0.0, 0.0, body->height*3.0/4.0);
     glEnd();
 
     glBegin(GL_TRIANGLES);
-    glColor3f(0.0,1.0,0.0);  // Сделали сторону  светло зеленой
-    glVertex3f((body->length/2), (body->width/2), 0.0);
-    glVertex3f(-(body->length/2), (body->width/2), 0.0);
-    glVertex3f(0.0, 0.0, body->height);
+    glColor3f(0.0, 1.0, 0.0);  // Сделали сторону  светло зеленой
+    glVertex3f((body->length / 2), (body->width / 2), -body->height/4.0);
+    glVertex3f(-(body->length / 2), (body->width / 2), -body->height/4.0);
+    glVertex3f(0.0, 0.0, body->height*3.0/4.0);
     glEnd();
 
     glBegin(GL_QUADS);// основание пирамиды
-    glColor3f(1.0,0.51,0.28); // сделали основание рыжим
-    glVertex3f((body->length/2), (body->width/2), 0.0);
-    glVertex3f((body->length/2), -(body->width/2), 0.0);
-    glVertex3f(-(body->length/2), -(body->width/2), 0.0);
-    glVertex3f(-(body->length/2), (body->width/2), 0.0);
+    glColor3f(1.0, 0.51, 0.28); // сделали основание рыжим
+    glVertex3f((body->length / 2), (body->width / 2), -body->height/4.0);
+    glVertex3f((body->length / 2), -(body->width / 2), -body->height/4.0);
+    glVertex3f(-(body->length / 2), -(body->width / 2), -body->height/4.0);
+    glVertex3f(-(body->length / 2), (body->width / 2), -body->height/4.0);
     glEnd();
 }
-
 void Idle() {
     long Time;
     static long OldTime = -1;
@@ -69,7 +68,7 @@ void screen(){
     glPushMatrix();
     glRotatef(2*acos(body->q.r)*180/M_PI,  body->q.i, body->q.j, body->q.k);
     /*glTranslated(body->x.x, body->x.y, body->x.z);*/
-    glTranslated(0, 0, (body->height*(1/4))); // вращение относительно цетра тяжести пирамиды
+    glTranslated(0, 0, 0); // вращение относительно цетра тяжести пирамиды
 
     glPolygonMode(GL_FRONT, GL_FILL);
     drawTriangle();
